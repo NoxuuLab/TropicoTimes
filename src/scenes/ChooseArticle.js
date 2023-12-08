b92107b83e2e8e0bab6c28713df19f749acdcba7
NoxuuLab <77098884+NoxuuLab@users.noreply.github.com>
2023-12-08 16:01:10 +0100

    drag and drop behaviour

--- src/scenes/ChooseArticle.js
+++ src/scenes/ChooseArticle.js
@@ -1,83 +1,28 @@
-console.log("Choose article loaded");
+export default class ChooseArticle extends Phaser.Scene {
+  constructor() {
+    super({ key: 'ChooseArticle' });
+  }
 
-class ChooseArticle extends Phaser.Scene
-{
-    constructor()
-    {
-        super({ key: 'ChooseArticle' });
-    }
+  preload() {
+    this.load.image('blocks', 'src/assets/introductionScene/blocks.png');
+  }
 
-    preload ()
-    {
-        this.load.image('bg', '../src/assets/introductionScene/background.png');
-        this.load.image('target', 'src/assets/introductionScene/target.png');
-        this.load.spritesheet('blocks', 'src/assets/introductionScene/blocks.png', { frameWidth: 64, frameHeight: 64 });
-    }
+  create() {
+    const blocks = this.add.image(100, 100, 'blocks').setInteractive({ draggable: true });
 
-    create ()
-    {
-        this.add.image(400, 300, 'bg');
+    this.input.setDraggable(blocks);
 
-        this.add.text(16, 16, 'Snap to Grid on Drag').setFontSize(24).setShadow(1, 1);
+    this.input.on('dragstart', function (pointer, gameObject) {
+      gameObject.setTint(0xff0000);
+    });
 
-        //  Create some 'drop zones'
-        this.add.image(640, 192, 'target').setOrigin(0, 0);
-        this.add.image(640, 320, 'target').setOrigin(0, 0);
-        this.add.image(640, 448, 'target').setOrigin(0, 0);
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+    });
 
-        //  The blocks we can drag
-        const block1 = this.add.sprite(64, 192, 'blocks', 1).setOrigin(0, 0);
-        const block2 = this.add.sprite(64, 320, 'blocks', 1).setOrigin(0, 0);
-        const block3 = this.add.sprite(64, 448, 'blocks', 1).setOrigin(0, 0);
-
-        block1.setInteractive({ draggable: true });
-        block2.setInteractive({ draggable: true });
-        block3.setInteractive({ draggable: true });
-
-        let over1 = false;
-        let over2 = false;
-        let over3 = false;
-
-        this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
-
-            //  This will snap our drag to a 64x64 grid
-
-            dragX = Phaser.Math.Snap.To(dragX, 64);
-            dragY = Phaser.Math.Snap.To(dragY, 64);
-
-            gameObject.setPosition(dragX, dragY);
-
-        });
-
-        //  The following code just checks to see if the gameObject is over
-        //  a zone when the drag ends and if so, we change frame and disable it
-
-        this.input.on('dragend', (pointer, gameObject) => {
-
-            const x = gameObject.x;
-            const y = gameObject.y;
-
-            if (x === 640 && y === 192 && !over1)
-            {
-                over1 = true;
-                gameObject.setFrame(0);
-                gameObject.disableInteractive();
-            }
-            else if (x === 640 && y === 320 && !over2)
-            {
-                over2 = true;
-                gameObject.setFrame(0);
-                gameObject.disableInteractive();
-            }
-            else if (x === 640 && y === 448 && !over3)
-            {
-                over3 = true;
-                gameObject.setFrame(0);
-                gameObject.disableInteractive();
-            }
-
-        });
-    }
+    this.input.on('dragend', function (pointer, gameObject) {
+      gameObject.clearTint();
+    });
+  }
 }
-
-export default ChooseArticle;
