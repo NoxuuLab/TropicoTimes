775f7e0a103cb5c278bc7227c561fa93ec57f81f
NoxuuLab <77098884+NoxuuLab@users.noreply.github.com>
2023-12-08 16:11:19 +0100

    drag and drop into container

--- src/scenes/ChooseArticle.js
+++ src/scenes/ChooseArticle.js
@@ -5,10 +5,14 @@ export default class ChooseArticle extends Phaser.Scene {
 
   preload() {
     this.load.image('blocks', 'src/assets/introductionScene/blocks.png');
+    this.load.image('container', 'src/assets/introductionScene/container.png');
+
   }
 
   create() {
+    const container = this.add.image(400, 300, 'container').setInteractive({ dropZone: true });
     const blocks = this.add.image(100, 100, 'blocks').setInteractive({ draggable: true });
+
 
     this.input.setDraggable(blocks);
 
@@ -23,6 +27,11 @@ export default class ChooseArticle extends Phaser.Scene {
 
     this.input.on('dragend', function (pointer, gameObject) {
       gameObject.clearTint();
+      if (!container.getBounds().contains(gameObject.x, gameObject.y)) {
+        // If not dropped in the container, reset to the initial position
+        gameObject.x = 100;
+        gameObject.y = 100;
+      }
     });
   }
 }
