acdc9e1307c61c1dfc2202ff8f9e84939f78db0a
NoxuuLab <77098884+NoxuuLab@users.noreply.github.com>
2024-01-18 18:37:53 +0100

    data.js 2 days, 6 articles

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -5,46 +5,47 @@ export default class Scene1 extends Phaser.Scene {
     constructor() {
         super({ key: 'Scene1' });
 
-         // Initialize an empty selectedData object
-         this.selectedData = {};
+        // Initialize variables
+        this.selectedData = {};
+        this.currentDay = 1;
+        this.currentArticle = 1;
+        this.currentTitleIndex = 0;
     }
 
     create() {
-        // Define your data
-        const data = myData;
+        // Create buttons for each article and display their first titles
+        for (let articleNum = 1; articleNum <= 6; articleNum++) {
+            const currentTitles = myData['day' + this.currentDay]['article' + articleNum];
 
-        // Choose random data
-        const selectedData = Phaser.Math.RND.pick(data);
-
-        // Create buttons for each title
-        data.forEach((entry, index) => {
-            const button = this.add.text(400, 100 * (index + 1), entry.title, { fontSize: '32px', fill: '#fff' })
+            const button = this.add.text(400, 50 * articleNum, currentTitles[0].title, { fontSize: '18px', fill: '#fff' })
                 .setOrigin(0.5)
                 .setInteractive();
-        
-           
-        
-            // Handle button click
+
+            // Add button click event
             button.on('pointerdown', () => {
+                // Update the title index for the clicked article
+                this.currentTitleIndex = (this.currentTitleIndex + 1) % currentTitles.length;
+
+                // Update the button text with the rotated title
+                button.setText(currentTitles[this.currentTitleIndex].title);
+
                 // Update selectedData with the clicked title
-                this.selectedData = entry;
+                this.selectedData = currentTitles[this.currentTitleIndex];
+
                 // Log the selected data
                 console.log('showData', this.selectedData);
-
-                button.setFill('#ff0000');
             });
+        }
+
+        // Add a "Publish" button to the scene
+        const publishButton = this.add.text(600, 500, 'Publish', { fontSize: '32px', fill: '#fff' })
+            .setOrigin(0.5)
+            .setInteractive();
+
+        // Handle publish button click
+        publishButton.on('pointerdown', () => {
+            // Move to Scene2 and pass data using init
+            this.scene.start('Scene2', { selectedData: this.selectedData });
         });
-        
-
-    // Add a "Publish" button to the scene
-    const publishButton = this.add.text(600, 500, 'Publish', { fontSize: '32px', fill: '#fff' })
-        .setOrigin(0.5)
-        .setInteractive();
-
-    // Handle publish button click
-    publishButton.on('pointerdown', () => {
-        // Move to Scene2 and pass data using init
-        this.scene.start('Scene2', { selectedData: this.selectedData });
-    });
-}
+    }
 }
--- src/scenes/data.js
+++ src/scenes/data.js
@@ -1,6 +1,20 @@
 // data.js
-export const myData = [
-    { title: "Dictator sucks", effect: -1 },
-    { title: "Dictator is awesome", effect: +2 },
-    // Add more data entries as needed
-];
+export const myData = {
+    day1: {
+      article1: [{ title: 'Dictator Sucks 1', effect: -1 }, { title: 'Dictator is Cool 1', effect: -2 }, { title: 'Dictator is Cool 1-2', effect: -5 }],
+      article2: [{ title: 'Dictator Sucks 2', effect: -1 }, { title: 'Dictator is Cool 2', effect: 2 }, { title: 'Dictator is Cool 2 2', effect: 4 }],
+      article3: [{ title: 'Dictator Sucks 3', effect: -1 }, { title: 'Dictator is Cool 5', effect: 4 }, { title: 'Dictator is Cool 6', effect: 2 }],
+      article4: [{ title: 'Dictator Sucks 4', effect: -1 }, { title: 'Dictator is Cool 7', effect: -3 }, { title: 'Dictator is Cool 8', effect: -1 }],
+      article5: [{ title: 'Dictator Sucks 5', effect: -1 }, { title: 'Dictator is Cool 9', effect: 1 }, { title: 'Dictator is Cool 10', effect: 5 }],
+      article6: [{ title: 'Dictator Sucks 6', effect: -1 }, { title: 'Dictator is Cool 11', effect: 0 }, { title: 'Dictator is Cool 12', effect: -2 }]
+    },
+    day2: {
+      article1: [{ title: 'Dictator Sucks 7', effect: -1 }, { title: 'Dictator is Cool 13', effect: -2 }, { title: 'Dictator is Cool 14', effect: -5 }],
+      article2: [{ title: 'Dictator Sucks 8', effect: -1 }, { title: 'Dictator is Cool 15', effect: 2 }, { title: 'Dictator is Cool 16', effect: 4 }],
+      article3: [{ title: 'Dictator Sucks 9', effect: -1 }, { title: 'Dictator is Cool 17', effect: 4 }, { title: 'Dictator is Cool 18', effect: 2 }],
+      article4: [{ title: 'Dictator Sucks 10', effect: -1 }, { title: 'Dictator is Cool 19', effect: -3 }, { title: 'Dictator is Cool 20', effect: -1 }],
+      article5: [{ title: 'Dictator Sucks 11', effect: -1 }, { title: 'Dictator is Cool 21', effect: 1 }, { title: 'Dictator is Cool 22', effect: 5 }],
+      article6: [{ title: 'Dictator Sucks 12', effect: -1 }, { title: 'Dictator is Cool 23', effect: 0 }, { title: 'Dictator is Cool 24', effect: -2 }]
+    }
+  };
+
