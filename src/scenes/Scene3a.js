d5f66223dd185bc333142e9093d2ba5616e49c69
NoxuuLab <77098884+NoxuuLab@users.noreply.github.com>
2024-01-20 00:09:22 +0100

    copie scene3 (works with myData) as scene3a

---
+++ src/scenes/Scene3a.js
@@ -0,0 +1,26 @@
+// Scene3.js
+export default class Scene3 extends Phaser.Scene {
+  constructor() {
+    super({ key: 'Scene3' });
+  }
+
+  init(data) {
+    // Data passed from Scene2
+    console.log('Scene 3: Received Data in init:', data.publishData);
+
+    // Store the received data in the scene's property
+    this.publishData = data.publishData;
+  }
+
+  create() {
+    console.log('Scene 3 created!');
+
+    // Calculate the sum of titleAmplification
+    const sumAmplifiedPost = this.publishData.reduce((sum, data) => sum + data.amplifiedPost, 0);
+
+    // Display the sum on the screen
+    const textStyle = { fontSize: '24px', fill: '#fff' };
+    this.add.text(400, 300, `Sum of Title Amplification: ${sumAmplifiedPost}`, textStyle)
+      .setOrigin(0.5);
+  }
+}
